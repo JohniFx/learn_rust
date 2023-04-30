e67bd502cd5e0659ba34566d9bc7550391c22585
JohniFx <janos_torok@yahoo.com>
2023-04-30 16:55:00 +0200

    before cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::io;
-fn another_function (x: i32 ){
-    println!("I am an other function");
-}
 
 fn main() {
     another_function(5);
 
+}
 
+fn another_function (x: i32 ){
+    println!("I am an other function");
+}
+
+fn variables(){
     // variables and mutability
     let mut x = 5;
     println!("Value of x is {x}");
